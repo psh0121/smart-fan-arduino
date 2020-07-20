4e77871be29730d0293fd0c8d923390075631567
psh0121 <tngus2057@naver.com>
2020-07-21 01:11:35 +0900

    버튼을 누를때마다 꺼졌다 켜짐 (first_one_button 수정)
    - 이전에 usb 케이블 연결시 바로 켜져버리는 현상에서 케이블을 연결해도 바로 켜지는 것이 아닌 꺼져있는 상태로 유지하도록 개선.
    - 여러번 버튼을 눌러야 인식이 되는 on/off 스위치를 한 번 누르면 꺼졌다 켜질 수 있도록 하며, 이전모다는 모터가 잘 돌아감.
    - 레지스터 발열이 있음.

---
+++ second_one_button/second_one_button.ino
@@ -0,0 +1,34 @@
+int DCmotor = 11; // 'DC모터'에 11값 입력 (디지털 핀 위치)
+int Button = 12; // '푸시버튼'에 12값 입력 (디지털 핀 위치)
+int current_state = 0; // '현재 스위치 상태' 변수 선언
+int previous_state = 0; // '이전 스위치 상태' 변수 선언
+int motor_enabled = 0; // '모터 작동 여부 결정값' 변수 선언
+
+void setup() {
+  pinMode(DCmotor,OUTPUT); // DC모터를 출력모드로 설정
+  pinMode(Button,INPUT);  // 푸시버튼을 입력모드로 설정
+}
+
+void loop() {
+  current_state = digitalRead(Button); 
+  // 버튼에 입력되어진 값을 읽어 현재 스위치 상태 변수에 저장
+  
+  if((current_state == HIGH) && (previous_state == LOW)){
+    // 만약 현재 스위치 상태가 HIGH이면서, 이전 스위치 상태가 LOW인 경우
+    
+    motor_enabled = 1 - motor_enabled; 
+    // 계산한 값을 다시 motor_enabled에 저장
+    
+    delay(10); // 0.01초 딜레이
+  }
+  previous_state = current_state; 
+  // 현재 스위치 상태 값을 이전 스위치 값에 저장
+  
+  if(motor_enabled == 1){ // 모터 작동 여부 결정값이 1인 경우
+    digitalWrite(DCmotor,LOW); // 모터를 끔
+  }
+  else{
+    digitalWrite(DCmotor,HIGH); // 그외에는 모터를 킴
+  }
+}
+    
