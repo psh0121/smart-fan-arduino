7b67413965670e8b9f2e4a39a584584faa5aa171
박수현 <tngus2057@naver.com>
2020-08-21 23:31:40 +0900

    DC모터 드라이브를 사용해 모터가 돌아가도록 설정2 내용 수정
    - 1) 기존에 블루투스와 DC모터를 연동해 스마트폰에서 버튼을 누르면 꺼졌다 켜질 수 있도록 설정했으나 해당 코드로는 모터가 돌아가지 않아 코드를 설정
    - 2) 시작시에 모터가 처음에 돌아가지 않도록 analogWrite를 입력
    - 3) 블루투스 값을 받을 변수의 내용을 수정

--- dcmotor_driver_2/dcmotor_driver_2.ino
+++ dcmotor_driver_2/dcmotor_driver_2.ino
@@ -1,16 +1,21 @@
 #include <SoftwareSerial.h> // 블루투스 시리얼 통신 라이브러리 추가
 SoftwareSerial Blue(3, 2); // 블루투스 설정 bluetooth(Tx, Rx)
 
-int in1 = 7; // in1을 7번포트에 연결
-int in2 = 5; // in2를 5번 포트에 연결
+long int data; // android 휴대폰에서 전송된 번호 저장하는데 사용
+
+int motorA_A1 = 10; // 모터에 연결된 디지털 변수 motorA_A1 선언
+int motorA_B1 = 9; // 모터에 연결된 디지털 변수 motorA_B1 선언
 
 long int password1 = 92; // '전원이 켜질 때'의 변수값 선언
 long int password2 = 79; // '전원이 꺼질 때'의 변수값 선언
 
 void setup() {
 
-  pinMode(in1, OUTPUT); // in1을 출력모드로 설정
-  pinMode(in2, OUTPUT); // in2를 출력모드로 설정
+  pinMode(motorA_A1, OUTPUT); // motorA_A1을 출력모드로 설정
+  pinMode(motorA_B1, OUTPUT); // motorA_B1을 출력모드로 설정
+
+  analogWrite(motorA_A1, 0); //시작할때 모터가 돌아가지 않도록 설정
+  analogWrite(motorA_B1, 0);
 
   Serial.begin(9600); // 시리얼 통신을 위한 보드레이트 설정
   Blue.begin(9600); // 블루투스 통신 시작
@@ -18,27 +23,28 @@ void setup() {
 
 void loop() {
 
-  while(Blue.available() == 0); // 블루투스에서 데이터를 받지 못했다면 여기서 기다리기
+  while(Blue.available() == 0); // 블루투스에 데이터를 받지 못했다면 여기서 기다리기
 
-  if(Blue.available()) { // 만약 블루투스에서 데이터가 들어왔을 경우
+  if(Blue.available() > 0) { // 블루투스에 값이 들어오면
 
-    char in_data; // 데이터가 들어온 값을 저장할 변수 in_data
-    in_data = Blue.read(); // 블루투스에서 들어온 값을 in_data에 입력
+    data = Blue.parseInt(); // 블루투스 값을 'data'에 입력
+    }
 
-    if(in_data == password1) { // 만약 데이터 값과 전원이 들어왔을때의 값이 같다면
+    delay(400); // 0.4초 딜레이
 
-      digitalWrite(in1, HIGH); // in1을  HIGH로 설정
-      digitalWrite(in2, LOW); // in2를 LOW로 설정
+    if(data == password1) { // 만약 데이터값과 불이 켜질때의 값이 같다면
+
+      analogWrite(motorA_A1, 200); // 모터가 돌아가도록 설정
+      analogWrite(motorA_B1, 0);
 
       Serial.println("DCmotor ON"); // 시리얼 모니터에 "DCmotor ON" 출력
     }
 
-    else if(in_data == password2) { // 만약 데이터 값과 전원이 꺼졌을때의 값이 같다면 
+    else if(data == password2) { // 만약 데이터값과 불이 꺼질때의 값이 같다면
 
-      digitalWrite(in1, LOW); // in1을 LOW로 설정
-      digitalWrite(in2, LOW); // in2를 LOW로 설정
+      analogWrite(motorA_A1, 0); // 모터가 꺼지도록 설정
+      analogWrite(motorA_B1, 0);
 
       Serial.println("DCmotor OFF"); // 시리얼 모니터에 "DCmotor OFF" 출력
     }
   }
-}
