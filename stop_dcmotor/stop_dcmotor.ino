c038818daa66f2a64924232833d1b398a54f2715
박수현 <tngus2057@naver.com>
2020-09-04 01:03:23 +0900

    아두이노 우노에 부착되어있는 LED가 시리얼 모니터에 특정 값을 입력했을 경우 5초간 불빛이 켜졌다 꺼지도록 설정

--- stop_dcmotor/stop_dcmotor.ino
+++ stop_dcmotor/stop_dcmotor.ino
@@ -1,35 +1,30 @@
-long previousMillis = 0; 
-// Interval을 5초로 지정
-long interval = 5000; 
+long previousMillis = 0; // 이전 시간 변수 previousMillis 선언
+long interval = 5000; // 시간 간격 변수 interval 선언
 
-int led = 13;
-int sw = 5;
+int led = 13; // 아두이노 우노에 부착되어 있는 LED 13번 선언
 
 void setup() {
-        pinMode(led, OUTPUT);
-        pinMode(sw, INPUT_PULLUP);
-        Serial.begin(9600);
+  
+  pinMode(led, OUTPUT); // led를 출력모드로 설정
+  
+  Serial.begin(9600); // 시리얼 통신을 위한 보드레이트 설정
 }
 
-void loop() {
-        // 현재 시간 측정
-        unsigned long currentMillis = millis(); 
-        // 시리얼 입력값을 읽는다.
-        char c = Serial.read(); 
+  unsigned long currentMillis = millis(); // 현재 시간 측정
+  
+  char c = Serial.read(); //시리얼 정보를 받아 c에 내용 저장
         
-        if (digitalRead(sw) == LOW) {
-                previousMillis = currentMillis;  
-                digitalWrite(led, HIGH);
-        }
-        else {
-                // a라는 글자를 입력한 경우 Hello문구 출력
-                if (c == 'a') { 
-                Serial.println("Hello");
-                }
-        }
+  if (c == '1') { // 만약 c가 1이라면
+          
+    previousMillis = currentMillis; // 이전시간에 현재시간 값을 넣기  
+    digitalWrite(led, HIGH); // led가 켜지도록 설정
+    Serial.println("불이 켜졌다"); // 시리얼 모니터에 "불이 켜졌다" 출력
+  }
         
-        // LED가 켜진지 5초뒤에 LED를 끈다
-        if (currentMillis - previousMillis > interval){ 
-                digitalWrite(led, LOW);
-        }
+  //만약 현재시간에서 이전 시간을 빼었을때 interval(5000)값보다 크다면
+  // = 5초의 시간이 흘렀다면
+  if (currentMillis - previousMillis > interval){ 
+    digitalWrite(led, LOW); // led가 꺼지도록 설정
+    Serial.println("불이 꺼졌다"); // 시리얼 모니터에 "불이 꺼졌다" 출력
+  }
 }
